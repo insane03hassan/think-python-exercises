f57742b5a3f61036b838f8ed3145e643ddc80a18
insane03hassan <105046529+insane03hassan@users.noreply.github.com>
2026-05-29 22:20:00 -0700

    Add chapter 2 exercise solution in Python

---
+++ chapters/chapter2exercise_solution.py
@@ -0,0 +1,50 @@
+# 2.11.1: Asking a Virtual assistant
+
+import keyword
+import math
+#import maath
+print(keyword.kwlist)
+
+# 2.11.2
+
+#17 = n 
+#print(17)
+
+n = 17
+print(n)
+
+x = y = 1
+print(x)
+print(y)
+
+print("My name is Hassan Abdulwahab");
+
+#print("My name is Hassan Abdulwahab"). 
+
+#print(maath.sqrt(9))
+print(math.sqrt(9))
+
+
+# 2.11.3 
+
+# Part 1
+r = 5
+# radius is in cm
+sphere_volume = (4/3)* math.pi * r**3
+# volume is in cubic centimeters
+print(f"{sphere_volume} cubic centimeters ")
+
+# Part 2
+
+x = 42
+cos_squared = (math.cos(x))**2
+sin_squared = (math.sin(x))**2
+estimate = cos_squared + sin_squared
+print(estimate)
+
+# Part 3
+
+print((math.e)**2)
+print(math.pow(math.e, 2))
+print(math.exp(2))
+
